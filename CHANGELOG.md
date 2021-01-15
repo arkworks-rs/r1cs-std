6c452b8f55435241ccec58d88c23f9212cea5449
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-01-15 13:18:26 -0800

    Better scalar multiplication for Short Weierstrass curves (#40)
    Co-authored-by: Dev Ojha <ValarDragon@users.noreply.github.com>
    Co-authored-by: Pratyush Mishra <pratyushmishra@berkeley.edu>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,26 +1,27 @@
 ## Pending
 
 ### Breaking changes
-- #12 Make FpVar's ToBitsGadget output constant length bit vectors.
+- #12 Make the `ToBitsGadget` impl for `FpVar` output fixed-size
 
 ### Features
 
 
 ### Improvements
 - #5 Speedup BLS-12 pairing
-- #13 Add ToConstraintFieldGadget to ProjectiveVar
-- #15, #16 Allow cs to be none when converting a montgomery point into a twisted edwards point
-- #20 Add conditional select gadget to Uints
-- #21 Add Uint128
+- #13 Add `ToConstraintFieldGadget` to `ProjectiveVar`
+- #15, #16 Allow `cs` to be `None` when converting a Montgomery point into a Twisted Edwards point
+- #20 Add `CondSelectGadget` impl for `UInt`s
+- #21 Add `UInt128`
 - #22 Reduce density of `three_bit_cond_neg_lookup`
-- #23 Reduce allocations in Uints
+- #23 Reduce allocations in `UInt`s
 - #33 Speedup scalar multiplication by a constant
-- #35 Construct a FpVar from bits
-- #36 ImplementToConstraintFieldGadget for `Vec<Uint8>`
+- #35 Construct a `FpVar` from bits
+- #36 Implement `ToConstraintFieldGadget` for `Vec<Uint8>`
+- #40 Faster scalar multiplication for Short Weierstrass curves by relying on affine formulae
 
 ### Bug fixes
-- #8 Fix bug in three_bit_cond_neg_lookup when using a constant lookup bit
-- #9 Fix bug in short weierstrass projective curve point's to_affine method
+- #8 Fix bug in `three_bit_cond_neg_lookup` when using a constant lookup bit
+- #9 Fix bug in `short_weierstrass::ProjectiveVar::to_affine` 
 - #29 Fix `to_non_unique_bytes` for `BLS12::G1Prepared`
 - #34 Fix `mul_by_inverse` for constants
 - #42 Fix regression in `mul_by_inverse` constraint count
---
+++ src/groups/curves/short_weierstrass/non_zero_affine.rs
@@ -0,0 +1,132 @@
+use super::*;
+/// An affine representation of a prime order curve point that is guaranteed
+/// to *not* be the point at infinity.
+#[derive(Derivative)]
+#[derivative(Debug, Clone)]
+#[must_use]
+pub struct NonZeroAffineVar<
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+> where
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    /// The x-coordinate.
+    pub x: F,
+    /// The y-coordinate.
+    pub y: F,
+    #[derivative(Debug = "ignore")]
+    _params: PhantomData<P>,
+}
+
+impl<P, F> NonZeroAffineVar<P, F>
+where
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    pub(crate) fn new(x: F, y: F) -> Self {
+        Self {
+            x,
+            y,
+            _params: PhantomData,
+        }
+    }
+
+    /// Converts self into a non-zero projective point.
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn into_projective(&self) -> ProjectiveVar<P, F> {
+        ProjectiveVar::new(self.x.clone(), self.y.clone(), F::one())
+    }
+
+    /// Performs an addition without checking that other != ±self.
+    #[tracing::instrument(target = "r1cs", skip(self, other))]
+    pub(crate) fn add_unchecked(&self, other: &Self) -> Result<Self, SynthesisError> {
+        if [self, other].is_constant() {
+            let result =
+                (self.value()?.into_projective() + other.value()?.into_projective()).into_affine();
+            Ok(Self::new(F::constant(result.x), F::constant(result.y)))
+        } else {
+            let (x1, y1) = (&self.x, &self.y);
+            let (x2, y2) = (&other.x, &other.y);
+            // Then,
+            // slope lambda := (y2 - y1)/(x2 - x1);
+            // x3 = lambda^2 - x1 - x2;
+            // y3 = lambda * (x1 - x3) - y1
+            let numerator = y2 - y1;
+            let denominator = x2 - x1;
+            let lambda = numerator.mul_by_inverse(&denominator)?;
+            let x3 = lambda.square()? - x1 - x2;
+            let y3 = lambda * &(x1 - &x3) - y1;
+            Ok(Self::new(x3, y3))
+        }
+    }
+
+    /// Doubles `self`. As this is a prime order curve point,
+    /// the output is guaranteed to not be the point at infinity.
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn double(&self) -> Result<Self, SynthesisError> {
+        if [self].is_constant() {
+            let result = self.value()?.into_projective().double().into_affine();
+            // Panic if the result is zero.
+            assert!(!result.is_zero());
+            Ok(Self::new(F::constant(result.x), F::constant(result.y)))
+        } else {
+            let (x1, y1) = (&self.x, &self.y);
+            let x1_sqr = x1.square()?;
+            // Then,
+            // tangent lambda := (3 * x1^2 + a) / y1·;
+            // x3 = lambda^2 - 2x1
+            // y3 = lambda * (x1 - x3) - y1
+            let numerator = x1_sqr.double()? + &x1_sqr + P::COEFF_A;
+            let denominator = y1.double()?;
+            let lambda = numerator.mul_by_inverse(&denominator)?;
+            let x3 = lambda.square()? - x1.double()?;
+            let y3 = lambda * &(x1 - &x3) - y1;
+            Ok(Self::new(x3, y3))
+        }
+    }
+
+    /// Doubles `self` in place.
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn double_in_place(&mut self) -> Result<(), SynthesisError> {
+        *self = self.double()?;
+        Ok(())
+    }
+}
+
+impl<P, F> R1CSVar<<P::BaseField as Field>::BasePrimeField> for NonZeroAffineVar<P, F>
+where
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    type Value = SWAffine<P>;
+
+    fn cs(&self) -> ConstraintSystemRef<<P::BaseField as Field>::BasePrimeField> {
+        self.x.cs().or(self.y.cs())
+    }
+
+    fn value(&self) -> Result<SWAffine<P>, SynthesisError> {
+        Ok(SWAffine::new(self.x.value()?, self.y.value()?, false))
+    }
+}
+
+impl<P, F> CondSelectGadget<<P::BaseField as Field>::BasePrimeField> for NonZeroAffineVar<P, F>
+where
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    #[inline]
+    #[tracing::instrument(target = "r1cs")]
+    fn conditionally_select(
+        cond: &Boolean<<P::BaseField as Field>::BasePrimeField>,
+        true_value: &Self,
+        false_value: &Self,
+    ) -> Result<Self, SynthesisError> {
+        let x = cond.select(&true_value.x, &false_value.x)?;
+        let y = cond.select(&true_value.y, &false_value.y)?;
+
+        Ok(Self::new(x, y))
+    }
+}
