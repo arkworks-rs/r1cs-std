4f306e4dd6b92c2de41e7f19d08c6c0bbc53244f
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-01-14 20:12:51 -0800

    New scalar multiplication code

--- src/groups/curves/short_weierstrass/non_zero_affine.rs
+++ src/groups/curves/short_weierstrass/non_zero_affine.rs
@@ -4,7 +4,7 @@ use super::*;
 #[derive(Derivative)]
 #[derivative(Debug, Clone)]
 #[must_use]
-pub struct AffineVar<
+pub struct NonZeroAffineVar<
     P: SWModelParameters,
     F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
 > where
@@ -18,7 +18,7 @@ pub struct AffineVar<
     _params: PhantomData<P>,
 }
 
-impl<P, F> AffineVar<P, F>
+impl<P, F> NonZeroAffineVar<P, F>
 where
     P: SWModelParameters,
     F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
@@ -32,13 +32,119 @@ where
         }
     }
 
-    /// Returns the value assigned to `self` in the underlying
-    /// constraint system.
-    pub(crate) fn value(&self) -> Result<SWAffine<P>, SynthesisError> {
-        Ok(SWAffine::new(
-            self.x.value()?,
-            self.y.value()?,
-            false,
-        ))
+    /// Converts self into a non-zero projective point.
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn into_projective(&self) -> ProjectiveVar<P, F> {
+        ProjectiveVar::new(self.x.clone(), self.y.clone(), F::one())
+    }
+
+    /// Performs an addition without checking that other != ±self.
+    #[tracing::instrument(target = "r1cs", skip(self, other))]
+    pub(crate) fn add_unchecked(&self, other: &Self) -> Result<Self, SynthesisError> {
+        if [self, other].is_constant() {
+            let result =
+                (self.value()?.into_projective() + other.value()?.into_projective()).into_affine();
+            // Panic is the result is zero.
+            assert!(!result.is_zero());
+            Ok(Self::new(F::constant(result.x), F::constant(result.y)))
+        } else {
+            let cs = [self, other].cs();
+            let (x1, y1) = (&self.x, &self.y);
+            let (x2, y2) = (&other.x, &other.y);
+            // Then,
+            // slope lambda := (y2 - y1)/(x2 - x1);
+            // x3 = lambda^2 - x1 - x2;
+            // y3 = lambda * (x1 - x3) - y1
+            let numerator = y2 - y1;
+            let denominator = x2 - x1;
+            let lambda = F::new_witness(ns!(cs, "lambda"), || {
+                Ok(numerator.value()?
+                    * &denominator
+                        .value()?
+                        .inverse()
+                        .unwrap_or(P::BaseField::zero()))
+            })?;
+            lambda.mul_equals(&denominator, &numerator)?;
+            let x3 = lambda.square()? - x1 - x2;
+            let y3 = lambda * &(x1 - &x3) - y1;
+            Ok(Self::new(x3, y3))
+        }
+    }
+
+    /// Doubles `self`
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn double(&self) -> Result<Self, SynthesisError> {
+        if [self].is_constant() {
+            let result = self.value()?.into_projective().double().into_affine();
+            // Panic is the result is zero.
+            assert!(!result.is_zero());
+            Ok(Self::new(F::constant(result.x), F::constant(result.y)))
+        } else {
+            let cs = self.cs();
+            let (x1, y1) = (&self.x, &self.y);
+            let x1_sqr = x1.square()?;
+            let numerator = x1_sqr.double()? + &x1_sqr + P::COEFF_A;
+            let denominator = y1.double()?;
+            // Then,
+            // slope lambda := (3 * x1^2 + a) / y1·;
+            // x3 = lambda^2 - 2x1
+            // y3 = lambda * (x1 - x3) - y1
+            let lambda = F::new_witness(ns!(cs, "lambda"), || {
+                Ok(numerator.value()?
+                    * &denominator
+                        .value()?
+                        .inverse()
+                        .unwrap_or(P::BaseField::zero()))
+            })?;
+            lambda.mul_equals(&denominator, &numerator)?;
+
+            let x3 = lambda.square()? - x1.double()?;
+            let y3 = lambda * &(x1 - &x3) - y1;
+            Ok(Self::new(x3, y3))
+        }
+    }
+
+    /// Doubles `self` in place.
+    #[tracing::instrument(target = "r1cs", skip(self))]
+    pub(crate) fn double_in_place(&mut self) -> Result<(), SynthesisError> {
+        *self = self.double()?;
+        Ok(())
+    }
+}
+
+impl<P, F> R1CSVar<<P::BaseField as Field>::BasePrimeField> for NonZeroAffineVar<P, F>
+where
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    type Value = SWAffine<P>;
+
+    fn cs(&self) -> ConstraintSystemRef<<P::BaseField as Field>::BasePrimeField> {
+        self.x.cs().or(self.y.cs())
+    }
+
+    fn value(&self) -> Result<SWAffine<P>, SynthesisError> {
+        Ok(SWAffine::new(self.x.value()?, self.y.value()?, false))
+    }
+}
+
+impl<P, F> CondSelectGadget<<P::BaseField as Field>::BasePrimeField> for NonZeroAffineVar<P, F>
+where
+    P: SWModelParameters,
+    F: FieldVar<P::BaseField, <P::BaseField as Field>::BasePrimeField>,
+    for<'a> &'a F: FieldOpsBounds<'a, P::BaseField, F>,
+{
+    #[inline]
+    #[tracing::instrument(target = "r1cs")]
+    fn conditionally_select(
+        cond: &Boolean<<P::BaseField as Field>::BasePrimeField>,
+        true_value: &Self,
+        false_value: &Self,
+    ) -> Result<Self, SynthesisError> {
+        let x = cond.select(&true_value.x, &false_value.x)?;
+        let y = cond.select(&true_value.y, &false_value.y)?;
+
+        Ok(Self::new(x, y))
     }
 }
