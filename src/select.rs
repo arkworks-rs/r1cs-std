3e817f20c57cef34c9defd9deb856c26ea13b626
mmagician <marcin.gorny.94@protonmail.com>
2023-05-02 10:35:25 -0600

    start the implementation of SumOfConditions method

--- src/select.rs
+++ src/select.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 use ark_ff::Field;
-use ark_relations::r1cs::SynthesisError;
+use ark_relations::r1cs::{LinearCombination, SynthesisError};
 use ark_std::vec::Vec;
 /// Generates constraints for selecting between one of two values.
 pub trait CondSelectGadget<ConstraintF: Field>
@@ -32,8 +32,100 @@ where
         position: &[Boolean<ConstraintF>],
         values: &[Self],
     ) -> Result<Self, SynthesisError> {
+        let _ = sum_of_conditions(position, values);
         repeated_selection(position, values)
     }
+}
+
+fn count_ones(x: usize) -> usize {
+    // count the number of 1s in the binary representation of x
+    let mut count = 0;
+    let mut y = x;
+    while y > 0 {
+        count += y & 1;
+        y >>= 1;
+    }
+    count
+}
+
+/// Sum of conditions method 5.2 from https://github.com/mir-protocol/r1cs-workshop/blob/master/workshop.pdf
+fn sum_of_conditions<ConstraintF: Field, CondG: CondSelectGadget<ConstraintF>>(
+    position: &[Boolean<ConstraintF>],
+    values: &[CondG],
+) -> Result<CondG, SynthesisError> {
+    let m = values.len();
+    let n = position.len();
+
+    // Assert m is a power of 2, and n = log(m)
+    assert!(m.is_power_of_two());
+    assert_eq!(1 << n, m);
+
+    let mut selectors: Vec<LinearCombination<ConstraintF>> = Vec::with_capacity(m);
+
+    // fill the selectors vec with Boolean true entries
+    for _ in 0..m {
+        selectors.push(Boolean::constant(true).lc());
+    }
+
+    // let's construct the table of selectors.
+    // for a bit-decomposition (b_{n-1}, b_{n-2}, ..., b_0) of `power`:
+    // [
+    //      (b_{n-1} * b_{n-2} * ... * b_1 * b_0),
+    //      (b_{n-1} * b_{n-2} * ... * b_1),
+    //      (b_{n-1} * b_{n-2} * ... * b_0),
+    //      ...
+    //      (b_1 * b_0),
+    //      b_1,
+    //      b_0,
+    //      1,
+    // ]
+    // signal selectors[leafCount];
+    //
+    // the element of the selector table at index i is a product of `bits`
+    // e.g. for i = 5 == (101)_binary
+    // `selectors[5]` <== b_2 * b_0`
+    // we can construct the first `max_bits_in_power - 1` elements without products,
+    // directly from `bits`:
+    // e.g. for
+    // `selectors[1] <== b_0`
+    // `selectors[2] <== b_1`
+    // `selectors[4] <== b_2`
+    // `selectors[8] <== b_3`
+
+    // First element is true, but we've already filled it in.
+    // selectors[0] = Boolean::constant(true);
+    for i in 0..n {
+        selectors[1 << i] = position[i].lc();
+        for j in (1 << i) + 1..(1 << (i + 1)) {
+            selectors[j] = &selectors[1 << i] + &selectors[j - (1 << i)];
+        }
+    }
+
+    let mut selector_sums: Vec<LinearCombination<ConstraintF>> = Vec::with_capacity(m);
+    for i in 0..m {
+        for j in 0..m {
+            if i | j == j {
+                let counts = count_ones(j - i);
+                if counts % 2 == 0 {
+                    selector_sums[i] = &selector_sums[i] + &selectors[j];
+                } else {
+                    selector_sums[i] = &selector_sums[i] - &selectors[j];
+                };
+            }
+        }
+    }
+
+    let root: LinearCombination<ConstraintF> = LinearCombination::zero();
+    // var x = 0;
+    for i in 0..m {
+        root = &root + (values[i], selector_sums[i]);
+    }
+    // for (var i = 0; i < nextPow; i++) {
+    //     x += leaves[i] * selector_sums[i];
+    // }
+    // root <== x;
+
+    unimplemented!()
 }
 
 /// Repeated selection method 5.1 from https://github.com/mir-protocol/r1cs-workshop/blob/master/workshop.pdf
