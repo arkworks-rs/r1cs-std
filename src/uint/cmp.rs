1ec138f0563817cfcd0893adba935ae202974ba8
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-06-19 13:26:55 +0200

    PoC for UInt

--- src/uint/cmp.rs
+++ src/uint/cmp.rs
@@ -1,18 +1,48 @@
-use crate::cmp::CmpGadget;
-
 use super::*;
+use crate::cmp::CmpGadget;
+use crate::fields::fp::FpVar;
 
 impl<const N: usize, T: PrimUInt, F: PrimeField + From<T>> CmpGadget<F> for UInt<N, T, F> {
     fn is_ge(&self, other: &Self) -> Result<Boolean<F>, SynthesisError> {
         if N + 1 < ((F::MODULUS_BIT_SIZE - 1) as usize) {
             let a = self.to_fp()?;
             let b = other.to_fp()?;
-            let (bits, _) = (a - b + F::from(T::max_value()) + F::one())
-                .to_bits_le_with_top_bits_zero(N + 1)?;
-            Ok(bits.last().unwrap().clone())
+
+            let b_over_a = self.get_slack(other)?.to_fp()?;
+            let a_over_b = other.get_slack(self)?.to_fp()?;
+            
+            a_over_b.mul_equals(&b_over_a, &FpVar::zero())?;
+            (a + &b_over_a).enforce_equal(&(b + a_over_b))?;
+
+            b_over_a.is_zero()
         } else {
             unimplemented!("bit sizes larger than modulus size not yet supported")
         }
+    }
+}
+
+impl<const N: usize, T: PrimUInt, F: PrimeField> UInt<N, T, F> {
+    /// Return the difference between `to` and `self` if `self < to`, otherwise return zero.
+    ///
+    /// Note: the value is not constrained in any way!
+    fn get_slack(&self, to: &Self) -> Result<Self, SynthesisError> {
+        let expected_mode = match self.is_constant() && to.is_constant() {
+            true => AllocationMode::Constant,
+            false => AllocationMode::Witness,
+        };
+
+        UInt::new_variable(
+            self.cs().or(to.cs()),
+            || {
+                let (from, to) = (self.value()?, to.value()?);
+                if from < to {
+                    Ok(to - from)
+                } else {
+                    Ok(T::zero())
+                }
+            },
+            expected_mode,
+        )
     }
 }
 
@@ -47,6 +77,15 @@ mod tests {
         if !both_constant {
             assert!(cs.is_satisfied().unwrap());
         }
+
+        println!(
+            "{} x {}    => {}cs & {}vars",
+            a.is_constant(),
+            b.is_constant(),
+            cs.num_constraints(),
+            cs.num_variables()
+        );
+
         Ok(())
     }
 
@@ -138,7 +177,7 @@ mod tests {
 
     #[test]
     fn u128_gt() {
-        run_binary_random::<1000, 128, _, _>(uint_gt::<u128, 128, Fr>).unwrap()
+        run_binary_random::<10, 128, _, _>(uint_gt::<u128, 128, Fr>).unwrap()
     }
 
     #[test]
